8d6b548269f6888a691b8064c4765acda78c493d
Angelos Makris <sade-1995@hotmail.com>
2023-05-31 16:22:24 +0300

    finished the index.html with all the elements and links

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  root to: "flats#index"
   resources :flats
   # Defines the root path route ("/")
   # root "articles#index"
