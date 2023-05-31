6d4338f3fd0ec630c6489f56c34cd06b00837877
Angelos Makris <sade-1995@hotmail.com>
2023-05-31 13:23:08 +0300

    added flat model controller + seeds + migration

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,12 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+10.times do
+  flat = Flat.create!(
+    name: ['Light & Spacious Garden Flat London', "Sunny Villa in Corfu", "Gothic appartment with view in Nuremberg","Tree house in Amazon forest", "Beach house in Sagres"].sample,
+    address: Faker::Address.full_address,
+    description: ["A lovely summer feel for this spacious garden flat"," Two double bedrooms, open plan living area",  "large kitchen and a beautiful conservatory"].sample,
+    price_per_night: rand(30..60),
+    number_of_guests: rand(1..5))
+end
